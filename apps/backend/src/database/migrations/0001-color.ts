63c5e731ffb3271e25c7d186a7028005a6b5e3fe
Marek Hanzal <marek.hanzal@icloud.com>
2026-03-20 14:46:21 +0100

    Prepared migrations, yaaay

--- apps/backend/src/database/migrations/0001-color.ts
+++ apps/backend/src/database/migrations/0001-color.ts
@@ -1,7 +1,20 @@
-import { type Migration, sql } from "kysely";
+import type { Migration } from "kysely";
 
 export const ColorMigration: Migration = {
 	async up(db) {
-		//
+		await db.schema
+			.createTable("color")
+			.addColumn("id", "integer", (column) =>
+				column.primaryKey().autoIncrement().notNull(),
+			)
+			.addColumn("name", "text", (column) => column.notNull())
+			.execute();
+
+		await db.schema
+			.createIndex("color_name_uq")
+			.on("color")
+			.column("name")
+			.unique()
+			.execute();
 	},
 };
--- apps/backend/src/database/migrations/0002-car.ts
+++ apps/backend/src/database/migrations/0002-car.ts
@@ -1,7 +1,30 @@
-import { type Migration, sql } from "kysely";
+import type { Migration } from "kysely";
 
 export const CarMigration: Migration = {
 	async up(db) {
-		//
+		await db.schema
+			.createTable("car")
+			.addColumn("id", "integer", (column) =>
+				column.primaryKey().autoIncrement().notNull(),
+			)
+			.addColumn("colorId", "integer", (column) =>
+				column.notNull().references("color.id").onDelete("restrict").onUpdate("cascade"),
+			)
+			.addColumn("make", "text", (column) => column.notNull())
+			.addColumn("model", "text", (column) => column.notNull())
+			.addColumn("builtAt", "text", (column) => column.notNull())
+			.execute();
+
+		await db.schema
+			.createIndex("car_color_id_idx")
+			.on("car")
+			.column("colorId")
+			.execute();
+
+		await db.schema
+			.createIndex("car_make_model_idx")
+			.on("car")
+			.columns(["make", "model"])
+			.execute();
 	},
 };
