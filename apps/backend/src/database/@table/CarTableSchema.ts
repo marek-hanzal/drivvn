fed81817f2a8365dce01426d9a909648753c8919
Marek Hanzal <marek.hanzal@icloud.com>
2026-03-20 20:26:22 +0100

    Enhance CarTableSchema to enforce builtAt date validation
    - Introduced a refinement on the builtAt field to ensure the date is not older than four years from the current date using Luxon.
    - Updated the schema definition to maintain compatibility with OpenAPI specifications.

--- apps/backend/src/database/@table/CarTableSchema.ts
+++ apps/backend/src/database/@table/CarTableSchema.ts
@@ -1,3 +1,4 @@
+import { DateTime } from "luxon";
 import z from "zod";
 
 export const CarTableSchema = z
@@ -12,10 +13,28 @@ export const CarTableSchema = z
 		model: z.string().min(1).openapi({
 			description: "How cool the car is in the make's roaster",
 		}),
-		builtAt: z.coerce.date().openapi({
-			description: "Whe the car has been built",
-			type: "string",
-		}),
+		builtAt: z.coerce
+			.date()
+			.refine(
+				(value) => {
+					return (
+						value >=
+						DateTime.utc()
+							.minus({
+								years: 4,
+							})
+							.startOf("day")
+							.toJSDate()
+					);
+				},
+				{
+					message: "Car build date can not be older than four years",
+				},
+			)
+			.openapi({
+				description: "Whe the car has been built",
+				type: "string",
+			}),
 	})
 	.strip()
 	.openapi("Car", {
